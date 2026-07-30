16451c8dc061a175495a1025acb7e96be8da425d
mofeo7 <omolade.mofe@gmail.com>
2026-07-30 12:26:45 -0600

    Reverted write_byte logic to past iteration

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #define sensor_start_register 0x3B //ACCEL_XOUT_H register. All other ACCEL + GYRO registers follw immediately after
 
 // put function declarations here:
-int write_byte(uint8_t dev_address, uint8_t reg_address, const uint8_t *data); //Write a single byte to peripheral register
+int write_byte(uint8_t dev_address, uint8_t reg_address, const uint8_t *data, size_t length); //Write a single byte to peripheral register
 int write_burst(uint8_t dev_address, uint8_t reg_address, const uint8_t *data, size_t length); //Write a specified number of bytes to multiple adjaescent peripheral registers
 int read_byte(uint8_t dev_address, uint8_t reg_addess); //Read a byte from single peripheral register
 int read_burst(uint8_t dev_address, uint8_t reg_address, size_t length, uint8_t *buffer); //Read a specified number of bytes from multiple adjaescent peripheral registers
@@ -18,7 +18,7 @@ void setup() {
   Serial.begin(115200);
 
   uint8_t wake_cmd = 0X00;
-  write_byter(main_register, wake_register, &wake_cmd); //Deactivate sleep mode by writing to PWR_MGMT_1 register
+  write_byte(main_register, wake_register, &wake_cmd, 1); //Deactivate sleep mode by writing to PWR_MGMT_1 register
 }
 
 void loop() {
@@ -26,8 +26,9 @@ void loop() {
   uint8_t IMU_buffer[14];
   
   if (read_burst(main_register, sensor_start_register, 14, IMU_buffer) == 0){
+    
     for(size_t i = 0; i < 14; i++){
-      
+      Serial.println(IMU_buffer[i]);
     }
   }
 
@@ -35,11 +36,11 @@ void loop() {
 }
 
 // put function definitions here:
-int write_byte(uint8_t dev_address, uint8_t reg_address, const uint8_t *data) {
+int write_byte(uint8_t dev_address, uint8_t reg_address, const uint8_t *data, size_t length) {
   Wire.beginTransmission(dev_address); //Initialize comms with the peripheral's main chip
   Wire.write(reg_address); //Tell the peripheral's chip which register to write to
 
-  Wire.write(&data, length); //Send data array to the register
+  Wire.write(data, length); //Send data array to the register
   return Wire.endTransmission(); //End transmission sequence
 
   /* 
@@ -76,16 +77,16 @@ int read_burst(uint8_t dev_address, uint8_t reg_address, size_t length, uint8_t 
   Wire.write(reg_address);
   
   if (Wire.endTransmission() != 0){ //Check that Wire.endTransmission is successful i.e. returns 0
-    return -1 //Transmission error
+    return -1; //Transmission error
   }
 
   size_t bytes_received = Wire.requestFrom(dev_address, length);
   if(bytes_received != length){ //Check that the bytes available on I2C bus match the reqested burst length
-    return -2 //Length mismatch error
+    return -2; //Length mismatch error
   }
 
   //Loop through wire read 
-  for(size_t i = 0; i < length, i++){
+  for(size_t i = 0; i < length; i++){
     if (Wire.available()){
       buffer[i] = Wire.read();
     }
